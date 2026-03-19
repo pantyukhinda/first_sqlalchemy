6a6de7743eb22d7591c8ead53870ae9ff037d7e4
pantyukhinda <dmitriy.pantyukhin@mail.ru>
2026-03-19 10:37:07 +0500

    First Insert and Select by Core syntax

--- app/core/crud/__init__.py
+++ app/core/crud/__init__.py
@@ -1,8 +1,10 @@
-from .exec_stmt import sync_exec, async_exec
+from .exec_stmt import sync_exec, async_exec, sync_query, async_query
 from .table_manager import create_tables
 
 __all__ = [
     "sync_exec",
     "async_exec",
+    "sync_query",
+    "async_query",
     "create_tables",
 ]
--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 import asyncio
 
-from sqlalchemy import text
+from sqlalchemy import text, insert, select
 
 from workers import workers
-from core.crud import async_exec
+from core.crud import async_exec, sync_query
 from core.crud import create_tables
 
 
 if __name__ == "__main__":
     create_tables()
 
-    stmt = text("INSERT INTO workers (id, name) VALUES (1, 'Ivan',), (2, 'Stepan');")
+    # stmt = text(
+    #     "INSERT INTO workers (id, name) VALUES (1, 'Ivan'), (2, 'Stepan') RETURNING *;"
+    # )
+    # query = text("SELECT * FROM workers WHERE id > 3;")
+    stmt = insert(workers).values(
+        [
+            {"name": "Ivan"},
+            {"name": "Stepan"},
+        ]
+    )
+
+    query = select(workers)
     asyncio.run(async_exec(stmt=stmt))
+    result = sync_query(query=query)
+    print(result)
