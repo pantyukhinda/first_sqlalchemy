421f5bbfa6c9cf48d1b7829322c0c6e57e71ee22
pantyukhinda <dmitriy.pantyukhin@mail.ru>
2026-03-24 13:32:56 +0500

    Add some workers and resumes

--- app/data.py
+++ app/data.py
@@ -5,19 +5,38 @@ class Data:
     workers_data: list[Workers] = [
         Workers(name="Ivan"),
         Workers(name="Stepan"),
+        Workers(name="Petr"),
     ]
 
     resumes_data: list[Resumes] = [
         Resumes(
-            title="Ivan's resume",
-            compensation=100_000,
-            workload="parttime",
+            title="Python junior developer",
+            compensation=60_000,
+            workload="fulltime",
             worker_id=1,
         ),
         Resumes(
-            title="Stepan's resume",
-            compensation=150_000,
+            title="Python Developer",
+            compensation=70_000,
+            workload="fulltime",
+            worker_id=1,
+        ),
+        Resumes(
+            title="Python Data Engineer",
+            compensation=80_000,
+            workload="parttime",
+            worker_id=2,
+        ),
+        Resumes(
+            title="Data scientist",
+            compensation=90_000,
             workload="fulltime",
             worker_id=2,
+        ),
+        Resumes(
+            title="Python Analyst",
+            compensation=100_000,
+            workload="fulltime",
+            worker_id=3,
         ),
     ]
