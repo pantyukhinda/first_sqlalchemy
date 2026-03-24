a754e16dda2b68f9a698c2eb7f0d726f2b88dc87
pantyukhinda <dmitriy.pantyukhin@mail.ru>
2026-03-24 16:35:18 +0500

    Add __repr__ for Base model

--- app/core/metadata/metadata.py
+++ app/core/metadata/metadata.py
@@ -17,3 +17,7 @@ update_at = Annotated[
 class Base(DeclarativeBase):
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        cols = [f"{col}={getattr(self, col)}" for col in self.__table__.columns.keys()]
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
